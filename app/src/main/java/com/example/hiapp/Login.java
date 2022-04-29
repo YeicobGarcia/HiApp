f8a6b5100ab3c38c4308b5d3ba5447438de419d6
YeicobGarcia <jacobgarcia.jm@gmail.com>
2022-04-28 22:09:15 -0600

    Modificacion de acceso

--- app/src/main/java/com/example/hiapp/Login.java
+++ app/src/main/java/com/example/hiapp/Login.java
@@ -1,14 +1,105 @@
 package com.example.hiapp;
 
+import static com.example.hiapp.Registro.handleSSLHandshake;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
 
 public class Login extends AppCompatActivity {
+
+    public Button btnIngresar;
+    public Button btnRegistro;
+    public TextView txtPassword;
+    public TextView txtUsuario;
+
+    ProgressBar progressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        this.txtUsuario = findViewById(R.id.txtUsuario);
+        this.txtPassword= findViewById(R.id.txtPassword);
+        this.btnIngresar = findViewById(R.id.btnIngresar);
+        this.btnRegistro = findViewById(R.id.btnRegistro);
+        this.progressBar = findViewById(R.id.progress);
+
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                final String username, password;
+                username = String.valueOf(txtUsuario.getText());
+                password = String.valueOf(txtPassword.getText());
+
+                if(!username.equals("") && !password.equals("")) {
+
+                    //Start ProgressBar first (Set visibility VISIBLE)
+                    progressBar.setVisibility(View.VISIBLE);
+                    handleSSLHandshake();
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            //Starting Write and Read data with URL
+                            //Creating array for parameters
+                            String[] field = new String[2];
+                            field[0] = "username";
+                            field[1] = "password";
+                            //Creating array for data
+                            String[] data = new String[2];
+                            data[0] = username;
+                            data[1] = password;
+                            PutData putData = new PutData("https://192.168.1.10/LoginRegister/login.php", "POST", field, data);
+                            if (putData.startPut()) {
+                                if (putData.onComplete()) {
+                                    progressBar.setVisibility(View.GONE);
+                                    String result = putData.getResult();
+                                    //End ProgressBar (Set visibility to GONE)
+                                    if(result.equals("Login Success")){
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    else{
+                                        Toast.makeText(getApplicationContext(),result,Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+                            //End Write and Read data with URL
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Debe llenar todos los campos", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registro = new Intent(Login.this, Registro.class);
+                startActivity(registro);
+            }
+        });
+
     }
+
 }
