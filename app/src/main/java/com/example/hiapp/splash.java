bd2e8c27ea9f7f0cdc6e021987e6b7e2063ead63
Danika <danikadonis@gmail.com>
2022-04-30 20:51:38 -0600

    Gif Splash Screen

--- app/src/main/java/com/example/hiapp/splash.java
+++ app/src/main/java/com/example/hiapp/splash.java
@@ -2,7 +2,9 @@ package com.example.hiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class splash extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler;
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this, Login.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 3000);
+
     }
 }
